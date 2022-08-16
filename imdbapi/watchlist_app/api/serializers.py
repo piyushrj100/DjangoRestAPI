a1691b14fa0a62488148c0a7be09111bcb29bf6d
piyushrj100 <piyush1.bksc@gmail.com>
2022-08-17 00:17:11 +0530

    drf contd..

--- imdbapi/watchlist_app/api/serializers.py
+++ imdbapi/watchlist_app/api/serializers.py
@@ -5,6 +5,7 @@ from watchlist_app.models import Watchlist, StreamPlatform, Review
 
 
 class ReviewSerializer(serializers.ModelSerializer) :
+    review_user = serializers.StringRelatedField(read_only=True)
     
     class Meta :
         model = Review 
--- imdbapi/watchlist_app/api/views.py
+++ imdbapi/watchlist_app/api/views.py
@@ -12,6 +12,8 @@ from rest_framework.views import APIView
 from rest_framework import generics
 # from rest_framework import mixins 
 from rest_framework import viewsets
+from rest_framework.exceptions import ValidationError
+
 
 
 
@@ -27,10 +29,19 @@ class ReviewList(generics.ListCreateAPIView) :
 class ReviewCreate(generics.CreateAPIView) :
     serializer_class = ReviewSerializer
 
+    def get_queryset(self) :
+
+        return Review.objects.all()
+
     def perform_create(self, serializer):
         pk = self.kwargs['pk']
         movie = Watchlist.objects.get(pk=pk)
-        serializer.save(watchlist=movie)
+        review_user = self.request.user
+        review_queryset = Review.objects.filter(watchlist=movie, review_user=review_user)
+
+        if review_queryset.exists() :
+            raise ValidationError("You have already reviewed this movie!") 
+        serializer.save(watchlist=movie, review_user=review_user)
         # return super().perform_create(serializer)
     
 
@@ -58,6 +69,8 @@ class ReviewList(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericA
 
 '''
 
+
+'''
 class StreamPlatformVS(viewsets.ViewSet) :
     def list(self,request) :
         queryset = StreamPlatform.objects.all()
@@ -77,6 +90,12 @@ class StreamPlatformVS(viewsets.ViewSet) :
             return Response(serializer.data)
         else : 
             return Response(serializer.errors)
+'''
+#Using Model View Set | works with router |  We have the control of everything GET, PUT, PATCH, DELETE, HEAD, OPTIONS  
+
+class StreamPlatformVS(viewsets.ModelViewSet) :
+    queryset = StreamPlatform.objects.all()
+    serializer_class = StreamPlatformSerializer
     
 class StreamPlatformAV(APIView) :
     def get(self,request) :
@@ -90,6 +109,9 @@ class StreamPlatformAV(APIView) :
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors)
+
+
+
 
 class StreamPlatformDetailAV(APIView) :
     def get(self,request,pk) :
