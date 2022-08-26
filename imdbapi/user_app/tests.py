0db1e3e5b0029a4c1b048eda2f379ea25e1dc56f
piyushrj100 <piyush1.bksc@gmail.com>
2022-08-26 23:30:44 +0530

    automated testing

--- imdbapi/user_app/tests.py
+++ imdbapi/user_app/tests.py
@@ -1,3 +1,19 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from django.urls import reverse
+from rest_framework import status 
+from django.contrib.auth.models import User 
+from rest_framework.authtoken.models import Token
 
-# Create your tests here.
+class RegisterTestCase(APITestCase) :
+    def test_register(self) :
+        data = {
+            "username" : "testcase",
+            "email" : "testcase@example.com",
+            "password" : "pass@123",
+            "password2" : "pass@123"
+
+        }
+        response = self.client.post(reverse('register'),data)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED )
+
+
